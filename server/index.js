4cec1f92c94b93919cc248c0157ff985b6f02efa
Gowtham-P-15 <gowthamperiyasamy15@gmail.com>
2021-03-12 01:46:36 +0530

    db connect

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello Wor!')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true ,useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
-app.get('/login', (req, res) => {
-  res.send('login page')
-})
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Server is running on port: ${port}`);
+});
