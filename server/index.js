e2c75efe445d5f8efc65f4ceda69b37648348477
Gowtham-P-15 <gowthamperiyasamy15@gmail.com>
2021-03-11 23:23:35 +0530

    just try

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello Wor!')
 })
-
+app.get('/login', (req, res) => {
+  res.send('login page')
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
